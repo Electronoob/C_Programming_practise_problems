22aef0f2aac3cbc4e4927fb77bbbf703d9e4145f
JAYARAJ <jayarajemengineer@gmail.com>
2025-04-19 16:42:29 +0530

    Create library.h

---
+++ Pointers/Substring_position/library.h
@@ -0,0 +1,4 @@
+
+void get_input(char *main_string, char *substring);
+int substring_position(char *main_string, char *substring, int8_t *position);
+void print_output(char *main_string, char *substring, int8_t *position);
